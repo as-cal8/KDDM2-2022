e0e8f48d948bdc304199891bbb28bdc8f36d4179
unknown <alexander.seyr@gmx.at>
2022-11-26 15:38:14 +0100

    some plots and analyzing

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -15,6 +15,50 @@ def main():
     timestamp_list = [start + datetime.timedelta(hours=x) for x in range(len(df))]
     df['timestamps'] = timestamp_list
 
+    # mistake in hours numbering hour 6 = 1248 instances
+    #                                 9 = 1246 instances
+    # TODO find and correct such that every hour has 1247 instances
+
+    # hourly change in consumption
+    diffMain = np.append(np.array(df['mainGrid']), 0) - np.append(0,np.array(df['mainGrid']))
+    diffMain = diffMain[0:-1]
+    diffMain[0] = 0
+    diffOther = np.append(np.array(df['otherGrids']), 0) - np.append(0,np.array(df['otherGrids']))
+    diffOther = diffOther[0:-1]
+    diffOther[0] = 0
+
+    df['diffMain'] = diffMain
+    df['diffOther'] = diffOther
+
+    df_hourDiffMain = pd.DataFrame()
+    for hour in np.arange(24):
+        hourDiffList = df.loc[df['hourOfDay'] == hour]['diffMain'].values
+        print("hour: " + str(hour))
+        print("hour diff list len: " + str(np.size(hourDiffList)))
+        df_hourDiffMain[str(hour)] = hourDiffList
+
+    df_hourDiffMain.boxplot()
+    plt.show()
+
+    print("Hourly differences in consumption")
+
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.boxplot(diffMain)
+    ax1.set_title("main Grid")
+    ax2.boxplot(diffOther)
+    ax2.set_title("other Grids")
+    plt.show()
+
+    plt.plot(df.timestamps[1:], diffMain)
+    plt.show()
+
+    fig, (ax1, ax2) = plt.subplots(1,2)
+    ax1.boxplot(df['mainGrid'])
+    ax1.set_title('main grid')
+    ax2.boxplot(df['otherGrids'])
+    ax2.set_title('other grids')
+    plt.show()
+
     figure(figsize=(22, 5), dpi=80, linewidth=5)
     plt.plot(timestamp_list, df['mainGrid'],'r.')
     plt.plot(timestamp_list, df['otherGrids'], 'b.')
@@ -44,6 +88,4 @@ Make Data Locally stationary -> Window Functions
     -> to then apply univariate Models
         -> exp smoothing, ARMA, ARIMA, GARCH
     -> or ML models (RNN, LSTM)
-
-
 '''
